9a112076b612a5bb9d79a1911b2f8a3d1a19070d
John Doe <edgarq1215@gmail.com>
2024-07-22 11:04:54 -0400

    Edited Day 6 to handle edge cases.

--- Day 6 - Escaping th Maze/main.py
+++ Day 6 - Escaping th Maze/main.py
@@ -26,12 +26,18 @@ def front_is_clear():
 def right_is_clear():
     print("Is there no wall to the right of you?")
 
+looping = 0
 while not at_goal():
+    if looping >= 4:
+        move()
+        looping = 0
     if right_is_clear():
         turn_right()
         move()
+        looping += 1
     else:
         turn_left()
+        looping = 0
         
      
         
