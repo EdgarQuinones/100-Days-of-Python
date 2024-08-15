08f998fbe9ad9c936279d6d777054c4731c924b4
edgarq1215@gmail.com <edgarq1215@gmail.com>
2024-08-15 14:24:58 -0400

    Day 30 - Password manager IMPROVED Complete
    Today I learned about error handling and JSON files. I used these skilled to improve the Nato alphabet project, as well as the password manager. These are useful skills I see myself using in the future

--- Day 26 - NATO Alphabet/main.py
+++ Day 26 - NATO Alphabet/main.py
@@ -1,39 +1,41 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     # Access key and value
+#     pass
+#
+#
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# # Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     # Access index and row
+#     # Access row.student or row.score
+#     pass
+#
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
 
 import pandas
 
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# TODO 1. Create a dictionary in this format:
 example = {"A": "Alfa", "B": "Bravo"}
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-full_code = []
-name = input("Enter your name: ").upper()
-for name_letter in name:
-    for (letter, code) in nato_dict.items():
-        if letter == name_letter.upper():
-            full_code.append(code)
-            break
-better_full_code = [nato_dict[letter] for letter in name if letter in name]
-print(full_code)
+better_full_code = []
+
+valid_input = False
+while not valid_input:
+    try:
+        name = input("Enter your name: ").upper()
+        better_full_code = [nato_dict[letter] for letter in name]
+    except KeyError:
+        print("Error: Only Letters Allowed.")
+    else:
+        valid_input = True
+
 print(better_full_code)
