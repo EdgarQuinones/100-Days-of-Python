24b1fdcbeceb0fd0223f036fcaad14f107c3fe2b
edgarq1215@gmail.com <edgarq1215@gmail.com>
2024-08-18 18:10:09 -0400

    Day 31 - Flash Card Complete
    Finally finished the day 31 project. Was close to handling the solution alone but ended up using the walk through for the ending.

--- Day 31 - Flash Cards/main.py
+++ Day 31 - Flash Cards/main.py
@@ -4,59 +4,37 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
-to_review = []
-
-# ---------------------------- WORDS TO LEARN FILE ------------------------------- #
+current_card = {}
+to_learn = {}
 
 try:
-    # Find words_to_learn.csv
-    with open("words_to_learn.csv", mode="r") as file:
-        content = file.readlines()
-        to_learn = pandas.read_csv(file).to_dict()
+    data = pandas.read_csv("data/words_to_learn.csv")
 except FileNotFoundError:
-    # Create new file
-    words_to_learn_file = open("words_to_learn.csv", mode="w")
-
-    data = pandas.read_csv("data/japanese_words.csv")
+    original_data = pandas.read_csv("data/japanese_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
     to_learn = data.to_dict(orient="records")
-
-current_card = random.choice(to_learn)
 
 
 # ---------------------------- CREATE FLASH CARDS ------------------------------- #
 def next_card():
-    global current_card, flip_timer, to_learn
+    global current_card, flip_timer
 
     window.after_cancel(flip_timer)
 
-    if len(to_learn) > 0:
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="Japanese", fill="black")
+    canvas.itemconfig(card_word, text=current_card["Japanese"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
 
-        current_card = random.choice(to_learn)
-        canvas.itemconfig(card_title, text="Japanese", fill="black")
-        canvas.itemconfig(card_word, text=current_card["Japanese"], fill="black")
-        canvas.itemconfig(canvas_image, image=card_front)
-
-        flip_timer = window.after(3000, flip_card)
-    else:
-
-        for card in to_review:
-            words_to_learn_file.write(f"{card}\n")
-        exit()
+    flip_timer = window.after(3000, flip_card)
 
 
-def remove_card():
-    if len(to_learn) > 0:
-        to_learn.remove(current_card)
-        next_card()
+def is_known():
+    to_learn.remove(current_card)
 
-
-def review_card():
-    global to_review
-
-    to_review.append(current_card)
-
-    if len(to_learn) > 0:
-        to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
 
     next_card()
 
@@ -83,11 +61,11 @@ card_word = canvas.create_text(400, 300, text="Word", font=("Ariel", 60, "bold")
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_image = tk.PhotoImage(file="images/wrong.png")
-wrong_button = tk.Button(image=wrong_image, highlightthickness=0, bd=0, command=review_card)
+wrong_button = tk.Button(image=wrong_image, highlightthickness=0, bd=0, command=next_card)
 wrong_button.grid(column=0, row=1, padx=50)
 
 right_image = tk.PhotoImage(file="images/right.png")
-right_button = tk.Button(image=right_image, highlightthickness=0, bd=0, command=remove_card)
+right_button = tk.Button(image=right_image, highlightthickness=0, bd=0, command=is_known)
 right_button.grid(column=1, row=1, padx=50)
 
 next_card()
