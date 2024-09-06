389a2cf119b5e74aba8368c5769268535199b34a
UnEvolvedGod <50989656+EdgarQuinones@users.noreply.github.com>
2024-09-05 21:36:34 -0400

    Day 39 in progress

--- Day 39 - Cheap Flight Finder (Part 1)/data_manager.py
+++ Day 39 - Cheap Flight Finder (Part 1)/data_manager.py
@@ -23,7 +23,19 @@ class DataManager:
                     "iataCode": sheet_data[index]['iataCode'],
                 }
             }
+            # sheety_put_endpoint = f"{os.environ["SHEETY_ENDPOINT"]}/{index + 2}"
+            # response = requests.put(url=sheety_put_endpoint, headers=sheety_header, json=sheety_parameters)
+            # response.raise_for_status()
+
+    def update_prices(self, sheet_data):
+
+        for index in range(len(sheet_data)):
+            # for index in range(1):
+            sheety_parameters = {
+                "price": {
+                    "lowestPrice": sheet_data[index]['lowestPrice'],
+                }
+            }
             sheety_put_endpoint = f"{os.environ["SHEETY_ENDPOINT"]}/{index + 2}"
             response = requests.put(url=sheety_put_endpoint, headers=sheety_header, json=sheety_parameters)
             response.raise_for_status()
-            print(f"({index}). Call Sent")
--- Day 39 - Cheap Flight Finder (Part 1)/main.py
+++ Day 39 - Cheap Flight Finder (Part 1)/main.py
@@ -5,6 +5,7 @@ import requests
 from data_manager import DataManager
 from flight_search import FlightSearch
 from dotenv import load_dotenv
+from flight_data import FlightData
 
 load_dotenv()
 
@@ -17,15 +18,24 @@ sheety_header = {
 
 response = requests.get(url=sheety_endpoint, headers=sheety_header)
 response.raise_for_status()
+print(response.json())
 sheet_data = response.json()["prices"]
 cities = []
 flight_search = FlightSearch()
+flight_data = FlightData()
 data_manager = DataManager()
 for column in sheet_data:
     code = flight_search.get_code(column["city"])
     column['iataCode'] = code
-
 data_manager.update_sheet(sheet_data)
+
+for column in sheet_data:
+    destination = column['iataCode']
+    price = flight_data.get_price(destination)
+    column['Lowest Price'] = price
+
+data_manager.update_prices(sheet_data)
+
 
 # response = requests.post(url=sheety_endpoint, headers=sheety_header, json=sheety_parameters)
 # response.raise_for_status()
